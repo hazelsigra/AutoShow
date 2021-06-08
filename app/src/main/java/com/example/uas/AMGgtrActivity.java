fceee94f16acb5989d7678abcb5921028eb91f68
mdanielelel <shaquille.muhammad0608@gmail.com>
2021-06-08 19:54:30 +0700

    addLogin

---
+++ app/src/main/java/com/example/uas/AMGgtrActivity.java
@@ -0,0 +1,14 @@
+package com.example.uas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AMGgtrActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_a_m_ggtr);
+    }
+}
---
+++ app/src/main/java/com/example/uas/HrvActivity.java
@@ -0,0 +1,14 @@
+package com.example.uas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HrvActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hrv);
+    }
+}
